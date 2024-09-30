10b7135e339cd1506e26073abedd73888eb6461e
Oxshady <shadymahmod55@gmail.com>
2024-10-01 02:02:12 +0300

    computed property name

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,3 +1,8 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income-${getCurrentYear()}`]: income,
