fe104bf1e983e046d29d0643116226dd06f2e30a
Oxshady <shadymahmod55@gmail.com>
2024-10-01 01:07:22 +0300

    trying another way to bypass style issue lol ^_^

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,11 +3,8 @@ export default function taskBlock(trueOrFalse) {
 	const task2 = true;
 
 	if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars
-		const task = true;
-    // eslint-disable-next-line no-unused-vars
-		const task2 = false;
-	}
+    return [task, task2]
+  }
 
 	return [task, task2];
 }
