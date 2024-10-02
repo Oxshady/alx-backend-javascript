e36c5aad21dfd81a6003a43b09bde37dfa5aeb45
Oxshady <shadymahmod55@gmail.com>
2024-10-03 01:48:00 +0300

    promise ^_^

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+	return new Promise((resolve, reject) => {
+	if (success) {
+		resolve({ "status": 200, "body": "Success" });
+	}
+	else {
+		reject({ "error": "The fake API is not working currently" });
+	}
+}
+);
+}
