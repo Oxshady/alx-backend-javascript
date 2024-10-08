182ecf46f2a0f0439b55b667421cece328d713d5
Oxshady <shadymahmod55@gmail.com>
2024-10-09 02:24:44 +0300

    map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,10 @@ export default function getListStudentIds(arr) {
   if (typeof (arr) !== typeof ([])) {
     return [];
   }
-  return arr.map((i) => i.id);
+  return arr.map((i) => {
+    if (i.id) {
+      return i.id;
+    }
+    return null;
+  });
 }
