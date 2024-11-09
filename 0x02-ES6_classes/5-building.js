257aaff821ebee807bc8f0bd3e71577274702d8f
Oxshady <shadymahmod55@gmail.com>
2024-11-10 01:55:51 +0200

    Add Building class with sqft validation and abstract evacuationWarningMessage method , adding test

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof (sqft) !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
