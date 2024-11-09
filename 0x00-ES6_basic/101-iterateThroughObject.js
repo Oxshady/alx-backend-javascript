d0a363d7cc4e17b27f48c856b02c326ee78ec135
Oxshady <shadymahmod55@gmail.com>
2024-11-10 00:18:18 +0200

    feat: add function to iterate through iterable object and format output as string with delimiter '|'

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const allEmps = [];
+  for (const employee of reportWithIterator) {
+    allEmps.push(employee);
+  }
+  return allEmps.join('|');
+}
