0092683fca86abedc4fbbb200caa77fe1aadf435
Oxshady <shadymahmod55@gmail.com>
2024-11-10 01:03:23 +0200

    fix: improve type validation and error messages in HolbertonCourse class
    - Refactored type checking logic to use  instead of  for array validation.
    - Improved error messages to ensure correct feedback on invalid types for , , and  properties.
    - Ensured the constructor and setters validate input types correctly for better data integrity and error handling.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,50 +1,50 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) !== typeof (String())) {
-      throw (TypeError("Name must be a string"));
-    }
-    else if (typeof (length) !== typeof (Number())) {
-      throw (TypeError("Length must be a number"));
-    }
-    else if (typeof (students) !== typeof (Array())) {
-      throw (TypeError("Students must be a Array"));
-    }
-    else {
+      throw (TypeError('Name must be a string'));
+    } else if (typeof (length) !== typeof (Number())) {
+      throw (TypeError('Length must be a number'));
+    } else if (typeof (students) !== typeof ([])) {
+      throw (TypeError('Students must be a Array'));
+    } else {
       this._name = name;
       this._length = length;
       this._students = students;
     }
   }
+
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof (name) !== typeof (String())) {
-      throw (TypeError("Name must be a string"));
-    }
-    else {
+      throw (TypeError('Name must be a string'));
+    } else {
       this._name = name;
     }
   }
+
   get length() {
     return this._length;
   }
+
   set length(length) {
     if (typeof (length) !== typeof (Number())) {
-      throw (TypeError("Length must be a number"));
-    }
-    else {
+      throw (TypeError('Length must be a number'));
+    } else {
       this._length = length;
     }
   }
+
   get students() {
     return this._students;
   }
+
   set students(students) {
-    if (typeof (students) !== typeof (Array())) {
-      throw (TypeError("Students must be a Array"));
-    }
-    else {
+    if (typeof (students) !== typeof ([])) {
+      throw (TypeError('Students must be a Array'));
+    } else {
       this._students = students;
     }
   }
