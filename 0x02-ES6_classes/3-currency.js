2e9ed5222ac45d4a0ae64a68782f378276394b7f
Oxshady <shadymahmod55@gmail.com>
2024-11-10 01:06:42 +0200

    fix: improve type validation and error handling in Currency class
    - Refactored type checks to validate strings using  and corrected error messages for  and  properties.
    - Ensured the constructor and setters properly check input types to avoid invalid data.
    - Added method  to return a formatted string combining currency name and code.

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,40 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof (code) !== typeof (String())) {
+      throw (TypeError('Code must be a string'));
+    } else if (typeof (name) !== typeof (String())) {
+      throw (TypeError('Name must be a string'));
+    } else {
+      this._code = code;
+      this._name = name;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof (name) !== typeof (String())) {
+      throw (TypeError('Name must be a string'));
+    } else {
+      this._name = name;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof (code) !== typeof (String())) {
+      throw (TypeError('Code must be a string'));
+    } else {
+      this._code = code;
+    }
+  }
+
+  displayFullCurrency() {
+    return (`${this._name} ${this.code}`);
+  }
+}
