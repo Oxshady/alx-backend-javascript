b9067f8813e4ee512a7c4b090d73e65c3bf16d7e
Oxshady <shadymahmod55@gmail.com>
2024-11-10 02:33:15 +0200

    feat: Implement SkyHighBuilding class extending Building
    - Created SkyHighBuilding class that extends Building.
    - Added constructor to initialize sqft and floors attributes.
    - Implemented getter and setter for sqft and floors.
    - Overrode evacuationWarningMessage method to return custom message.

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
