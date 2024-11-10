eca837689cadc363290ded843e523560c38e31b3
Oxshady <shadymahmod55@gmail.com>
2024-11-10 03:17:31 +0200

    Fix constructor in StudentHolberton class, correct references to , and update  method.

---
+++ 0x02-ES6_classes/9-main.js
@@ -0,0 +1,10 @@
+
+import listOfStudents from "./9-hoisting.js";
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
+
+console.log(listPrinted)
