b1abea57a34ade9601f89b488b1e83a4abf10069
Oxshady <shadymahmod55@gmail.com>
2024-11-10 02:38:34 +0200

    change permission of the file

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,6 +1,6 @@
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
