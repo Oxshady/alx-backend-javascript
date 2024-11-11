a19cee7d8d6f552692fa47e67573a5dc9ac62a2e
Oxshady <shadymahmod55@gmail.com>
2024-11-11 16:42:55 +0200

    feat: add handleResponseFromAPI function to handle API responses
    - Created handleResponseFromAPI function that processes a given promise.
    - When the promise resolves, it returns an object with { status: 200, body: 'Success' }.
    - When the promise rejects, it returns a new Error object.
    - A log message Got a response from the API is printed regardless of resolution or rejection.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  })
-    .catch(() => new Error());
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => new Error()).finally(() => console.log('Got a response from the API'));
 }
