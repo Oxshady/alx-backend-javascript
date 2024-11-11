44101a2ff9b826a5afe84b158cc5fa61647ec4d7
Oxshady <shadymahmod55@gmail.com>
2024-11-11 17:47:39 +0200

    Implement signUpUser function to return user firstName and lastName as a resolved Promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-	return new Promise((resolve, reject)=>{
-		resolve({
-			firstName,
-			lastName
-		});
-	})
+  return new Promise((resolve) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+  });
 }
