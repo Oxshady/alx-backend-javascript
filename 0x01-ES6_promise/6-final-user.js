a9980a897b2a220b3e96d5ec481d66cde8092d3f
Oxshady <shadymahmod55@gmail.com>
2024-11-11 18:20:36 +0200

    Fix handleProfileSignup to correctly return status and value for settled promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,17 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(fileName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
+    (promises) => {
+      const statusValue = [];
+      promises.forEach((x) => {
+        statusValue.push({
+          status: x.status,
+          value: x.value,
+        });
+	});
+	return statusValue;
+    },
+  );
 }
