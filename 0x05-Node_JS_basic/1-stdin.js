16551519ded45e3147b8fd75559f02df3e393a1c
Oxshady <shadymahmod55@gmail.com>
2024-12-09 03:08:08 +0200

    feat: fix input stream handling and clean up code for user name input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,11 @@
-process.stdout.write('Welcome to ALX, what is your name?\n', (err) => {
-  if (err) console.log(err);
-});
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-  if (input) {
-    const data = input.toString().trim();
-    process.stdout.write(`Your name is: ${data}\n`, (err) => {
-      if (err) console.log(err);
-    });
-    process.stdin.pause();
-    process.stdin.end(() => {
-      process.stdout.write('This important software is now closing\n', (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    });
-  }
+  const data = input.toString().trim();
+
+  process.stdout.write(`Your name is: ${data}\n`);
+
+  process.stdin.end(() => {
+    process.stdout.write('This important software is now closing\n');
+  });
 });
