0043b1af069b0b9cd4020727520dad67f1fa78ff
Oxshady <shadymahmod55@gmail.com>
2024-12-09 03:38:00 +0200

    using event to achieve purpose

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,20 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to ALX, what is your name?\n', (err) => {
+  if (err) console.log(err);
+});
 
 process.stdin.on('data', (input) => {
-  const data = input.toString().trim();
+  if (input) {
+    const data = input.toString().trim();
+    process.stdout.write(`Your name is: ${data}`, (err) => {
+      if (err) console.log(err);
+    });
+  }
+});
 
-  process.stdout.write(`Your name is: ${data}\n`);
-
-  process.stdin.end(() => {
-    process.stdout.write('This important software is now closing\n');
+process.on('end', () => {
+  process.stdout.write('This important software is now closing\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
   });
 });
