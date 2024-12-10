1a741ccb0ae71a4b81bb79a9db0494550298bc89
Oxshady <shadymahmod55@gmail.com>
2024-12-10 02:43:17 +0200

    feat: add countStudents function to process student data
    - Implemented the countStudents function to read and process student data from a CSV file
    - Categorized students into CS and SWE based on their course
    - Printed the total number of students, CS students, and SWE students with their names

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  try {
+    let data = fs.readFileSync(path, { encoding: 'utf8' });
+    if (!data) {
+      return;
+    }
+    data = data.split('\n');
+    data.shift();
+    const helper = {
+      numberOfStudents: data.length,
+      csStudents: [],
+      sweStudents: [],
+      csStudentsCounts: 0,
+      sweStudentsCounts: 0,
+    };
+
+    for (let record of data) { // 'let' here to avoid redeclaration of 'record'
+      record = record.split(',');
+      // Use 'record[record.length - 1]' if .at() is not supported
+      if (record[record.length - 1].toLowerCase() === 'swe') {
+        helper.sweStudentsCounts += 1;
+        helper.sweStudents.push(record[0]);
+      } else if (record[record.length - 1].toLowerCase() === 'cs') {
+        helper.csStudentsCounts += 1;
+        helper.csStudents.push(record[0]);
+      }
+    }
+
+    console.log(`Number of students: ${helper.numberOfStudents}`);
+    if (helper.csStudentsCounts > 0) {
+      console.log(`Number of students in CS: ${helper.csStudentsCounts}. List: ${helper.csStudents.join(', ')}`);
+    }
+    if (helper.sweStudentsCounts > 0) {
+      console.log(`Number of students in SWE: ${helper.sweStudentsCounts}. List: ${helper.sweStudents.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
