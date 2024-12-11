2787935bde52cfc9df1c3d97272648520e3f4eee
Oxshady <shadymahmod55@gmail.com>
2024-12-11 05:19:53 +0200

    see is checker will change?

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,8 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
+describe('first number rounded', () => {
   it('add correctly', () => {
     assert.equal(calculateNumber(1.2, 1), 2, 'should be 2');
-    assert.equal(calculateNumber(1, 1.8), 3, 'should be 3');
-    assert.equal(calculateNumber(1.2, 1.7), 3, 'should be 3');
-    assert.equal(calculateNumber(1.6, 1.7), 4, 'should be 4');
-    assert.equal(calculateNumber(1, 1), 2, 'should be 2');
-  });
-  it('edge cases where passing string', () => {
-    assert.equal(calculateNumber('holberton', 2), NaN, 'should be NaN');
-    assert.equal(calculateNumber('holberton', 'School'), NaN, 'should be NaN');
-  });
-  it('edge cases where no passing for args', () => {
-    assert.equal(calculateNumber(), NaN, 'should be NaN');
-    assert.equal(calculateNumber(1), NaN, 'should be NaN');
   });
 });
