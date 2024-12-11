9b8fa933119887b5c47cc623331a83249b1812c4
Oxshady <shadymahmod55@gmail.com>
2024-12-11 05:22:38 +0200

    unit testing

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,3 +6,9 @@ describe('first number rounded', () => {
     assert.equal(calculateNumber(1.2, 1), 2, 'should be 2');
   });
 });
+
+describe('second number rounded', () => {
+  it('add correctly', () => {
+    assert.equal(calculateNumber(1, 1.2), 2, 'should be 2');
+  });
+});
